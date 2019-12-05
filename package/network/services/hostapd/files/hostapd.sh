1392cd5ceacdf9b57233728c91f3552ccb6c48a4
Daniel Golle <daniel@makrotopia.org>
2019-12-05 18:35:36 +0100

    hostapd: re-introduce process tracking
    Before commit 60fb4c92b6 ("hostapd: add ubus reload") netifd was
    tracking hostapd/wpa_supplicant and restarting wifi in case of a
    process crash. Restore this behaviour by tracking the PIDs of
    hostapd and wpa_supplicant.
    Also make sure hostapd and/or wpa_supplicant have been started before
    emmitting ubus calls to them using ubus wait_for.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>
    Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

--- package/network/services/hostapd/files/hostapd.sh
+++ package/network/services/hostapd/files/hostapd.sh
@@ -924,19 +924,21 @@ wpa_supplicant_run() {
 
 	_wpa_supplicant_common "$ifname"
 
-	/usr/sbin/wpa_supplicant -B -s \
-		${network_bridge:+-b $network_bridge} \
-		-P "/var/run/wpa_supplicant-${ifname}.pid" \
-		-D ${_w_driver:-wext} \
-		-i "$ifname" \
-		-c "$_config" \
-		-C "$_rpath" \
-		"$@"
+	ubus wait_for wpa_supplicant.$phy
+	ubus call wpa_supplicant.$phy config_add "{ \
+		\"driver\": \"${_w_driver:-wext}\", \"ctrl\": \"$_rpath\", \
+		\"iface\": \"$ifname\", \"config\": \"$_config\" \
+		${network_bridge:+, \"bridge\": \"$network_bridge\"} \
+		${hostapd_ctrl:+, \"hostapd_ctrl\": \"$hostapd_ctrl\"} \
+		}"
 
 	ret="$?"
 	wireless_add_process "$(cat "/var/run/wpa_supplicant-${ifname}.pid")" /usr/sbin/wpa_supplicant 1
 
 	[ "$ret" != 0 ] && wireless_setup_vif_failed WPA_SUPPLICANT_FAILED
+
+	local supplicant_pid=$(ubus call service list '{"name": "hostapd"}' | jsonfilter -l 1 -e "@['hostapd'].instances['supplicant-${phy}'].pid")
+	wireless_add_process "$supplicant_pid" "/usr/sbin/wpa_supplicant" 1
 
 	return $ret
 }
