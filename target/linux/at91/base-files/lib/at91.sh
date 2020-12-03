pavelpernicka/openwrt-at91-mod

commit 2a64a6540c04ea09e959f476fe92c56f869b204c

at91: add sama5d2-icp board
Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

target/linux/at91/base-files/lib/at91.sh
@@ -52,6 +52,9 @@ at91_board_detect() {
 	*"SAMA5D2 Xplained")
 		name="sama5d2_xplained"
 		;;
+	*"SAMA5D2 ICP")
+		name="sama5d2_icp"
+		;;
 	*"SAMA5D4 Xplained")
 		name="sama5d4_xplained"
 		;;

configs/sama5d2_icp_defconfig
@@ -1,0 +1,50 @@
+CONFIG_TARGET_at91=y
+CONFIG_TARGET_at91_sama5d2=y
+CONFIG_TARGET_at91_sama5d2_DEVICE_at91-sama5d2_icp=y
+CONFIG_HAS_SUBTARGETS=y
+CONFIG_HAS_DEVICES=y
+CONFIG_TARGET_BOARD="at91"
+CONFIG_TARGET_SUBTARGET="sama5d2"
+CONFIG_TARGET_PROFILE="DEVICE_at91-sama5d2_icp"
+CONFIG_TARGET_ARCH_PACKAGES="arm_cortex-a5"
+CONFIG_DEFAULT_TARGET_OPTIMIZATION="-O3 -pipe -mcpu=cortex-a5 -mfpu=vfpv4-d16 -mfloat-abi=hard"
+CONFIG_CPU_TYPE="cortex-a5"
+# CONFIG_TARGET_ROOTFS_SQUASHFS is not set
+# CONFIG_TARGET_IMAGES_GZIP is not set
+CONFIG_DEVEL=y
+CONFIG_EXTERNAL_KERNEL_TREE=""
+CONFIG_KERNEL_GIT_CLONE_URI=""
+CONFIG_KERNEL_GIT_LOCAL_REPOSITORY="https://github.com/linux4sam/linux-at91.git"
+CONFIG_KERNEL_GIT_REF="linux4sam_6.1-rc1"
+CONFIG_NEED_TOOLCHAIN=y
+CONFIG_TOOLCHAINOPTS=y
+CONFIG_LIBC_USE_GLIBC=y
+CONFIG_GDB=y
+CONFIG_USE_GLIBC=y
+CONFIG_LIBC="glibc"
+CONFIG_TARGET_SUFFIX="gnueabi"
+CONFIG_IMAGEOPT=y
+CONFIG_PREINITOPT=y
+CONFIG_INITOPT=y
+CONFIG_VERSIONOPT=y
+CONFIG_VERSION_DIST="Openwrt"
+CONFIG_VERSION_NUMBER=""
+CONFIG_VERSION_CODE="linux4sam_6.1-rc1"
+CONFIG_VERSION_REPO="http://downloads.openwrt.org/releases/18.06.2/"
+CONFIG_VERSION_MANUFACTURER="Microchip Technology Inc"
+CONFIG_VERSION_MANUFACTURER_URL="www.microchip.com"
+CONFIG_VERSION_BUG_URL=""
+CONFIG_VERSION_SUPPORT_URL=""
+CONFIG_VERSION_PRODUCT="SAMA5D2_ICP_board"
+CONFIG_VERSION_HWREV="sama5d27"
+CONFIG_VERSION_FILENAMES=y
+CONFIG_VERSION_CODE_FILENAMES=y
+CONFIG_PER_FEED_REPO=y
+CONFIG_PER_FEED_REPO_ADD_DISABLED=y
+CONFIG_PER_FEED_REPO_ADD_COMMENTED=y
+CONFIG_FEED_packages=y
+CONFIG_FEED_luci=y
+CONFIG_FEED_routing=y
+CONFIG_FEED_telephony=y
+CONFIG_PACKAGE_luci=y
+CONFIG_PACKAGE_luci-ssl-openssl=y

package/boot/at91bootstrap/Makefile
@@ -106,6 +106,12 @@ define AT91Bootstrap/sama5d2_ptc_eksd_uboot
   BUILD_DEVICES:=at91-sama5d2_ptc_ek
 endef
 
+define AT91Bootstrap/sama5d2_icpsd_uboot
+  TITLE:=AT91Bootstrap for SAMA5D2 ICP (SDCard)
+  BUILD_SUBTARGET:=sama5
+  BUILD_DEVICES:=at91-sama5d2_icp
+endef
+
 AT91BOOTSTRAP_TARGETS := \
     sama5d2_xplaineddf_uboot \
     sama5d2_xplaineddf_qspi_uboot \
@@ -118,7 +124,8 @@ AT91BOOTSTRAP_TARGETS := \
 	sama5d27_som1_eksd_uboot \
 	sama5d27_som1_ekqspi_uboot \
 	sama5d2_ptc_eknf_uboot \
-    sama5d2_ptc_eksd_uboot
+    sama5d2_ptc_eksd_uboot \
+    sama5d2_icpsd_uboot
 
 define Build/Compile
 	+$(MAKE) $(PKG_JOBS) -C $(PKG_BUILD_DIR) \

package/boot/uboot-at91/Makefile
@@ -104,6 +104,12 @@ define U-Boot/sama5d2_ptc_ek_mmc
   BUILD_DEVICES:=at91-sama5d2_ptc_ek
 endef
 
+define U-Boot/sama5d2_icp_mmc
+  NAME:=SAMA5D2 ICP (SDCard)
+  BUILD_SUBTARGET:=sama5
+  BUILD_DEVICES:=at91-sama5d2_icp
+endef
+
 UBOOT_TARGETS := \
 	at91sam9m10g45ek_nandflash \
 	at91sam9x5ek_nandflash \
@@ -117,7 +123,8 @@ UBOOT_TARGETS := \
     sama5d27_som1_ek_mmc \
     sama5d27_som1_ek_spiflash \
 	sama5d2_ptc_ek_nandflash \
-	sama5d2_ptc_ek_mmc
+	sama5d2_ptc_ek_mmc \
+	sama5d2_icp_mmc
 
 define Build/Compile
   +$(MAKE) $(PKG_JOBS) -C $(PKG_BUILD_DIR) \

package/microchip/at91dtoverlay/Makefile
@@ -50,11 +50,18 @@ define AT91DTOverlay/sama5d27_som1_ek
   BUILD_DEVICES:=at91-sama5d27_som1_ek
 endef
 
+define AT91DTOverlay/sama5d2_icp
+  TITLE:=AT91DTOverlay for SAMA5D2 ICP
+  BUILD_SUBTARGET:=sama5
+  BUILD_DEVICES:=at91-sama5d2_icp
+endef
+
 AT91DTOVERLAY_TARGETS := \
     sama5d2_xplained \
     sama5d3_xplained \
     sama5d4_xplained \
-    sama5d27_som1_ek
+    sama5d27_som1_ek \
+    sama5d2_icp
 
 define Build/Compile
   $(MAKE) DTC=$(LINUX_DIR)/scripts/dtc/dtc KERNEL_DIR=$(LINUX_DIR) -C $(PKG_BUILD_DIR) $(BUILD_DEVICES:at91-%=%)_dtbos

target/linux/at91/image/sama5.mk
@@ -69,3 +69,11 @@ define Device/at91-sama5d4_xplained
   $(Device/evaluation-sdimage)
 endef
 TARGET_DEVICES += at91-sama5d4_xplained
+
+define Device/at91-sama5d2_icp
+  $(Device/evaluation-dtb)
+  DEVICE_TITLE := Microchip SAMA5D2 ICP
+  KERNEL_SIZE := 6144k
+  $(Device/evaluation-sdimage)
+endef
+TARGET_DEVICES += at91-sama5d2_icp

commit 2337dafb8f2cd69ad7392c4e478ba40f51c1661e

at91: fix board name
Fix SAMA5D2-ICP board name.

Fixes: 2a64a6540c04 ("at91: add sama5d2-icp board")
Signed-off-by: Claudiu Beznea <claudiu.beznea@microchip.com>

target/linux/at91/base-files/lib/at91.sh
@@ -55,7 +55,7 @@ at91_board_detect() {
 	*"SAMA5D2 Xplained")
 		name="sama5d2_xplained"
 		;;
-	*"SAMA5D2 ICP")
+	*"Microchip SAMA5D2-ICP")
 		name="sama5d2_icp"
 		;;
 	*"SAMA5D4 Xplained")