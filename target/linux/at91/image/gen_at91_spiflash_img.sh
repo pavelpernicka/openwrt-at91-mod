b2d1205aee1f2aaa19c469718188df3035ec6202
Pavel Pernička <pernicka.pa@gmail.com>
2021-03-31 20:34:38 +0200

    Added support for spi flash booting on at91sam9x5 chips

---
+++ target/linux/at91/image/gen_at91_spiflash_img.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+COMBINEDFILE="$6"
+BOOTLOADER="$3"
+KERNEL="$5"
+ROOTFSR="$4"
+UBOOT="$2"
+UBOOT_ENV="$1"
+
+newsize=$((16777216)) #16 MB flash
+rm $COMBINEDFILE
+
+# offset musí začínat na začátku erase blocku
+dd conv=notrunc bs=1 if=$BOOTLOADER of=$COMBINEDFILE seek=0 
+dd conv=notrunc bs=1 if=$UBOOT_ENV of=$COMBINEDFILE seek=$((0x00005000))
+dd conv=notrunc bs=1 if=$UBOOT of=$COMBINEDFILE seek=$((0x00008000))
+dd conv=notrunc bs=1 if=$KERNEL of=$COMBINEDFILE seek=$((0x000a8000))
+dd conv=notrunc bs=1 if=$ROOTFSR of=$COMBINEDFILE seek=$((0x00418000))
+
+filesize=$(stat -c "%s" $COMBINEDFILE)
+padcount=$((newsize - filesize))
+dd if=/dev/zero ibs=1 count="$padcount" | tr "\000" "\377" >> $COMBINEDFILE
+echo "Done!"
